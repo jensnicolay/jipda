f74b7e2b4245605c81d9d54ad6c34adf6251e3ed
jnicolay <jens.nicolay@gmail.com>
2016-08-10 15:08:09 +0200

    fixed HaltKont.equals bug

--- jsCesk.js
+++ jsCesk.js
@@ -1439,7 +1439,7 @@ function jsCesk(cc)
   HaltKont.prototype.equals =
     function (x)
     {
-      return this instanceof HaltKont;
+      return x instanceof HaltKont;
     }
   HaltKont.prototype.addresses =
     function ()
