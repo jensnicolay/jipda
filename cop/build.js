2360f4ddfc572fabf88479fa502344ebf6042019
jnicolay <jens.nicolay@gmail.com>
2017-06-16 15:20:03 +0200

    cop setup

--- cop/build.js
+++ cop/build.js
@@ -2,42 +2,27 @@
 
 load("../lib/esprima.js");
 
-var console = {log:print};
+const ast0src = read("../prelude.js");
 
-Array.prototype.toString =
-  function ()
-  {
-    if (this.length === 0)
-    {
-      return "[]";
-    }
-    var s = "[";
-    var i = 0;
-    for (; i < this.length - 1; i++)
-    {
-      s += this[i] + ","; 
-    }
-    s += this[i] + "]";
-    return s;   
-  };
+var console = {log:print}
 
-  load("../common.js");
-  load("../countingStore.js");
-  load("../agc.js");
-  load("../lattice.js");
-  load("../abstLattice1-2.js");
-  load("../concLattice.js");
-  load("../ast.js");
-  load("../jsCesk.js");
-  load("../tag-alloc.js");
-  load("../conc-alloc.js");
-  load("../benv.js");
-  load("../object.js");
-  load("../test.js");
-  
-  load("purityAnalysis.js");
-  load("protopurity.js");
-  
-  load("test/purityTests.js");
+load("../common.js");
+load("../countingStore.js");
+load("../agc.js");
+load("../lattice.js");
+load("../conc-lattice.js");
+load("../conc-alloc.js");
+load("../conc-kalloc.js");
+load("../type-lattice.js");
+load("../tag-alloc.js");
+load("../aac-kalloc.js");
+load("../ast.js");
+load("../benv.js");
+load("../object.js");
+load("../jsCesk.js");
+load("../jipda.js");
+load("../test.js");
 
-// ~/git/v8/out/native/d8 --use_strict --max_old_space_size=4096 --shell build.js
+load("cop.js");
+
+load("test/copTests.js");
